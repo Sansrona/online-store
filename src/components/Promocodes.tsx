3ce25654091ffef12052a7be5b423e72d1aad73d
rin <prettysunshine468@gmail.com>
2023-01-10 01:28:40 +0600

    feat: add styles for cart page

--- src/components/Promocodes.tsx
+++ src/components/Promocodes.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { PromocodeType } from "../api/types";
+import React, {useEffect, useState} from "react";
+import {PromocodeType} from "../api/types";
 
 type PromocodesTypes = {
   setDiscount: (promo: PromocodeType) => void;
@@ -8,16 +8,16 @@ type PromocodesTypes = {
 };
 
 const promocodes: PromocodeType[] = [
-  { name: "rs", value: 10, title: "Rolling Scopes School" },
-  { name: "epam", value: 10, title: "EPAM systems" },
-  { name: "new-year", value: 20, title: "Happy New Year!" },
+  {name: "rs", value: 10, title: "Rolling Scopes School"},
+  {name: "epam", value: 10, title: "EPAM systems"},
+  {name: "new-year", value: 20, title: "Happy New Year!"},
 ];
 
 const Promocodes: React.FC<PromocodesTypes> = ({
-  setDiscount,
-  dropDiscount,
-  discountList,
-}) => {
+                                                 setDiscount,
+                                                 dropDiscount,
+                                                 discountList,
+                                               }) => {
   const [promoText, setPromoText] = useState("");
   const [promo, setPromo] = useState<PromocodeType | null>();
   const [isActive, setIsActive] = useState(false);
@@ -48,15 +48,17 @@ const Promocodes: React.FC<PromocodesTypes> = ({
   }, [promoText]);
 
   return (
-    <div>
+    <div className="pb-8 flex flex-col items-center">
       {discountList.length > 0 &&
         discountList.map((discount) => {
           return (
-            <div key={discount.name}>
+            <div className="flex items-center gap-3 py-2 px-4 rounded-xl border border-slate-300 mt-4 mb-4"
+                 key={discount.name}>
               <p>
                 {discount?.title} - {discount?.value}%
               </p>
               <button
+                className="px-2 py-1 rounded bg-red-300 hover:bg-red-400 transition-colors text-sm uppercase font-medium"
                 onClick={() => {
                   onPromoDrop(discount.name);
                 }}
@@ -72,16 +74,20 @@ const Promocodes: React.FC<PromocodesTypes> = ({
         value={promoText}
         name="promocode"
         id="promocode"
-        placeholder="promocode"
+        placeholder="Enter promocode"
+        className="border p-1 px-3 rounded-lg"
       />
 
       {isActive && (
-        <div>
+        <div className="flex items-center gap-3 py-2 px-4 rounded-xl border border-slate-300 mt-4 mb-4">
           <p>
             {promo?.title} - {promo?.value}%
           </p>
           {promo && !discountList.map((x) => x.name).includes(promo.name) && (
-            <button onClick={onPromoAdd}>Add</button>
+            <button
+              className="px-2 py-1.5 rounded bg-green-200 hover:bg-green-300 transition-colors text-sm uppercase font-medium"
+              onClick={onPromoAdd}>Add
+            </button>
           )}
         </div>
       )}
